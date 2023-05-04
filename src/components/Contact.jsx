ec9e56d1128a642bc9a7a5c7489db010bcef0b46
SamSyntax <agzawodowy@gmail.com>
2023-05-04 16:05:41 +0200

    Layout fixes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -34,7 +34,7 @@ function Contact() {
   }
 
   return (
-    <div id='Contact' className='section text-white w-screen h-fit md:h-[1100px] flex justify-center items-start bg-black/20 mt-12 '>
+    <div id='Contact' className='section text-white w-screen h-fit lg:h-[1100px] flex justify-center items-start bg-black/20 mt-12 '>
       <ToastContainer
         icon={false}
         position='top-right'
@@ -51,38 +51,37 @@ function Contact() {
       <div className='flex flex-col justify-center items-center p-12 mb-0 gap-5  w-full backdrop-blur-3xl '>
         <h1 className='text-4xl font-semibold'>Skontaktuj się z nami!</h1>
         <div>
-          <div className='w-screen flex flex-col justify-center items-start gap-8 px-12 py-12 md:px-24 md:flex-row md:items-center'>
-            <div className='flex justify-center items-center gap-8 text-2xl'>
+          <div className=' flex flex-col justify-center items-start gap-8 px-12 py-12 md:px-24 lg:flex-row md:items-center w-[60vw]'>
+            <div className='flex justify-start items-center gap-8 md:text-2xl p-auto m-auto text-sm w-[350px]'>
               <HiPhone className='w-[36px] h-[36px]' />
               <p>111-222-333</p>
             </div>
-            <div className='flex justify-center items-center gap-8 text-2xl'>
-              <HiLocationMarker className='w-[36px] h-[36px]' />
+            <div className='flex justify-start items-center gap-8 md:text-2xl p-auto m-auto text-sm w-[350px]'>
+              <HiLocationMarker className='sm:w-[72px] sm:h-[72px] md:w-[36px] md:h-[36px]' />
               <p>
-                00-230, Zielonka <br />
-                Ul. Zięby 22/3
+                00-230, Zielonka <br className='sm:hidden md:block' /> Ul. Zięby 22/3
               </p>
             </div>
-            <div className='flex justify-center items-center gap-8 text-2xl'>
+            <div className='flex justify-start items-center gap-8 md:text-2xl p-auto m-auto text-sm w-[350px]'>
               <HiMail className='w-[36px] h-[36px]' />
               <p>wwe-tuning@o2.pl</p>
             </div>
           </div>
         </div>
-        <div className='grid  grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1 justify-center items-center'>
+        <div className='grid  grid-cols-1 grid-rows-2 lg:grid-cols-2 lg:grid-rows-1 justify-center items-center'>
           <form className='flex flex-col justify-center items-center md:items-start  p-12 gap-5 ' ref={form} onSubmit={sendEmail}>
             <input
               name='email_id'
               required
               placeholder='Email'
-              className='text-zinc-250 text-lg p-0 border border-zinc-250 rounded-md bg-black/20 backdrop-blur-2xl w-[75vw] md:max-w-[450px]'
+              className='text-zinc-250 text-lg p-0 border border-zinc-250 rounded-md bg-black/20 backdrop-blur-2xl w-[75vw] md:max-w-[500px]'
               type='email'
             />
             <input
               name='name_id'
               required
               placeholder='Imię i Nazwisko'
-              className='text-zinc-250 text-lg p-0 border border-zinc-250 rounded-md bg-black/20 backdrop-blur-2xl w-[75vw] md:max-w-[450px]'
+              className='text-zinc-250 text-lg p-0 border border-zinc-250 rounded-md bg-black/20 backdrop-blur-2xl w-[75vw] md:max-w-[500px]'
               type='text'
             />
 
@@ -90,7 +89,7 @@ function Contact() {
               name='message_id'
               required
               placeholder='Wiadomość'
-              className='text-zinc-250 text-lg p-0 border border-zinc-250 rounded-md bg-black/20 backdrop-blur-2xl w-[75vw] md:max-w-[450px] h-[20rem] text-start'
+              className='text-zinc-250 text-lg p-0 border border-zinc-250 rounded-md bg-black/20 backdrop-blur-2xl w-[75vw] md:max-w-[500px] h-[20rem] text-start'
               type='text'
             />
             <input
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -12,7 +12,7 @@ function Services() {
 
   return (
     <ScrollTrigger className='flex flex-col justify-between' onEnter={() => setCounterOn(true)} onExit={() => setCounterOn(false)}>
-      <div className='flex justify-center items-center w-screen lg:my-2 my-[8rem] '>
+      <div className='flex justify-center items-center w-screen lg:my-2 my-[8rem] md:my-[18rem] '>
         <div className='w-full h-[200px] md:h-[400px] lg:h-[200px]  bg-black/20 lg:flex lg:justify-around backdrop-blur-3xl grid grid-cols-2 grid-rows-2 justify-center items-center '>
           <img className='md:scale-50 scale-sm' src={volkswagen} alt='' />
           <img className='md:scale-50 scale-sm' src={bmw} alt='' />
@@ -22,29 +22,29 @@ function Services() {
       </div>
       <div
         id='Services'
-        className='h-screen w-screen grid md:grid-cols-2 md:grid-rows-2 sm:grid-cols-1 sm:grid-rows-2  px-12 lg:px-24 gap-12  text-zinc-200'
+        className='h-screen w-screen grid md:grid-cols-2 md:grid-rows-2 sm:grid-cols-1 sm:grid-rows-2 px-4 lg:px-24 gap-12   text-zinc-200'
       >
         <div className='col-span-2 md:flex md:items-center justify-center items-end hidden'>
           <h1 className='text-5xl font-semibold'>Czym się zajmujemy</h1>
         </div>
-        <div className='flex flex-col justify-center lg:text-7xl'>
+        <div className='flex flex-col justify-center md:justify-start lg:text-7xl'>
           <div className='flex items-center gap-8 md:justify-start '>
-            <BsClipboard2Check className='md:w-[80px] md:h-[80px] md:block hidden' />
-            <h1 className='text-4xl font-semibold text-zinc-200'>
+            <BsClipboard2Check className='lg:w-[80px] lg:h-[80px] lg:block hidden' />
+            <h1 className='md:text-4xl text-5xl font-semibold text-zinc-200'>
               {counterOn && <CountUp start={0} end={200} duration={3} delay={0.2} />}+ Zadowolonych klientów
             </h1>
           </div>
           <br />
           <div className='flex items-center gap-8 md:justify-start'>
-            <BsEvFront className='md:w-[80px] md:h-[80px] md:block hidden' />
-            <h1 className='text-4xl font-semibold text-zinc-200'>
+            <BsEvFront className='lg:w-[80px] lg:h-[80px] lg:block hidden' />
+            <h1 className='md:text-4xl text-5xl font-semibold text-zinc-200'>
               {counterOn && <CountUp start={0} end={10} duration={3} delay={0.2} />}+ Lat doświadczenia
             </h1>
           </div>
         </div>
-        <div className='flex flex-col justify-center  items-center'>
+        <div className='flex flex-col justify-start  items-center'>
           <h1 className='md:hidden flex text-5xl font-semibold pb-12 text-center'>Czym się zajmujemy</h1>
-          <p className='text-left text-lg lg:text-xl'>
+          <p className='text-left text-lg lg:text-2xl'>
             WWE-TUNING oferuje kompleksowe usługi chip tuningu samochodów dla marek takich jak Audi, BMW, Mercedes-Benz oraz Volkswagen. Usługi
             obejmują chip tuning, optymalizację układu wtryskowego, diagnostykę samochodową, programowanie sterowników oraz montaż akcesoriów
             tuningowych. Zespół firmy składa się z doświadczonych specjalistów, którzy gwarantują najwyższą jakość usług i pełną satysfakcję klientów.
