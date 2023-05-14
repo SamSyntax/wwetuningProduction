1e872cfd5a4b582a7996dc035fd1e229a13acbfc
SamSyntax <agzawodowy@gmail.com>
2023-05-14 21:52:16 +0200

    Style changes

--- src/components/License.jsx
+++ src/components/License.jsx
@@ -2,12 +2,12 @@ import React from "react"
 
 function License() {
   return (
-    <div className='flex flex-col h-screen w-screen mt-[24rem] md:mt-0 md:mb-24 gap-12 items-center justify-center px-24'>
+    <div className='flex flex-col h-screen w-screen mt-[24rem] lg:mt-0 md:mb-24 gap-12 items-center justify-center px-24'>
       <div className=' flex items-center justify-center'>
         <h1 className='text-zinc-200 text-4xl font-semibold'>Licencja WinOLS</h1>
       </div>
-      <div className=' flex flex-row justify-around items-center gap-5'>
-        <div className='w-[50vw] h-full'>
+      <div className=' flex md:flex-row flex-col justify-around items-center md:gap-5 gap-12'>
+        <div className='md:w-[50vw] w-full h-full'>
           <a href='https://www.evc.de/en/check_evc_license.asp?k=%2bcvnUR%2frr%2bEuBFY%2fdmnN9A%3d%3d'>
             <img
               className='relative w-full lg:w-2/3 rounded-lg hover:rounded-[1rem] opacity-75 hover:scale-105 transition-all ease-in-out duration-700'
@@ -16,8 +16,8 @@ function License() {
             />
           </a>
         </div>
-        <div className='w-1/2'>
-          <p className='text-zinc-200 text-sm md:text-lg'>
+        <div className='md:w-1/2 w-full'>
+          <p className='text-zinc-200 text-md md:text-lg'>
             Jesteśmy profesjonalną firmą zajmującą się tuningiem samochodów, a nasze usługi opierają się na najnowocześniejszych technologiach. W celu
             zapewnienia najwyższej jakości i skuteczności naszych prac, korzystamy z najlepszego oprogramowania do edycji map w sterownikach silnika -
             WinOLS. Dzięki temu możemy zapewnić naszym klientom pełną funkcjonalność programu, a także legalne i zgodne z prawem korzystanie z tej
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -14,10 +14,10 @@ function Services() {
     <ScrollTrigger className='flex flex-col justify-between' onEnter={() => setCounterOn(true)} onExit={() => setCounterOn(false)}>
       <div className='flex justify-center items-center w-screen lg:my-2 my-[8rem] md:my-[18rem] '>
         <div className='w-full h-[200px] md:h-[400px] lg:h-[200px]  bg-black/20 lg:flex lg:justify-around backdrop-blur-3xl grid grid-cols-2 grid-rows-2 justify-center items-center '>
-          <img className='md:scale-50 scale-sm' src={volkswagen} alt='' />
-          <img className='md:scale-50 scale-sm' src={bmw} alt='' />
-          <img className='md:scale-50 scale-sm' src={mercedes} alt='' />
-          <img className='md:scale-50 scale-sm' src={audi} alt='' />
+          <img className='md:scale-50 scale-sm hover:scalemark transition-all ease-in-out duration-700' src={volkswagen} alt='' />
+          <img className='md:scale-50 scale-sm hover:scalemark transition-all ease-in-out duration-700' src={bmw} alt='' />
+          <img className='md:scale-50 scale-sm hover:scalemark transition-all ease-in-out duration-700' src={mercedes} alt='' />
+          <img className='md:scale-50 scale-sm hover:scalemark transition-all ease-in-out duration-700' src={audi} alt='' />
         </div>
       </div>
       <div
