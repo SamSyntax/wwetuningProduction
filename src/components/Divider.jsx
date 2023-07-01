69386301a504dc7c68fe2c70e2bb1d53ed295f0f
SamSyntax <agzawodowy@gmail.com>
2023-07-01 21:41:41 +0200

    Another IOS fixes

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -6,7 +6,7 @@ import audi from "../assets/audi.png"
 
 function Divider() {
   return (
-    <div className='flex justify-center items-center w-screen  md:my-36 md:mb-0 my-24'>
+    <div className='flex justify-center items-center w-screen  md:my-36 md:mb-0 my-36'>
       <div className='w-full h-[200px] md:h-[400px] lg:h-[200px]  bg-black/20 lg:flex lg:justify-around backdrop-blur-3xl grid grid-cols-2 grid-rows-2 justify-center items-center '>
         <img className='md:scale-50 scale-sm hover:scalemark transition-all ease-in-out duration-700' src={volkswagen} alt='' />
         <img className='md:scale-50 scale-sm hover:scalemark transition-all ease-in-out duration-700' src={bmw} alt='' />
